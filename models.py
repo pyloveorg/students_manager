a83f92279cc967a3df5c07ce6839c689c7cadfa6
Kamila Urbaniak <kamila.urbaniak26@gmail.com>
2018-03-15 23:52:10 +0100

    do poprawienia login

--- models.py
+++ models.py
@@ -8,10 +8,12 @@ from sqlalchemy import Column
 from sqlalchemy.types import Integer
 from sqlalchemy.types import String
 from sqlalchemy.types import Boolean
+from sqlalchemy.types import DateTime
+from main import bcrypt
 
 from main import db
 
-from wtforms import Form, validators, StringField, PasswordField
+from wtforms import Form, validators, StringField, PasswordField, BooleanField, SubmitField
 from wtforms.fields.html5 import EmailField
 
 
@@ -27,11 +29,20 @@ class User(db.Model, UserMixin):
     password = Column(String(200), default='')
     admin = Column(Boolean, default=False)
 
+    def get_id(self):
+        return self.id
+
     def is_active(self):
         """
         Returns if user is active.
         """
         return self.active
+
+    def is_authenticated(self):
+        return True
+
+    def is_anonymous(self):
+        return False
 
     def is_admin(self):
         """
@@ -39,8 +50,52 @@ class User(db.Model, UserMixin):
         """
         return self.admin
 
+    @staticmethod
+    def make_password(self, plaintext):
+        return bcrypt.generate_password_hash(plaintext).encode('utf-8')
+
+    def check_password(self, password):
+        if not self.password or not password:
+            return False
+        return bcrypt.check_password_hash(self.password, password).decode('utf-8')
+
+    @classmethod
+    def create(self, username, password, **kwargs):
+        return User(
+            username=username,
+            password=User.make_password(password),
+            **kwargs
+        )
+
+    @staticmethod
+    def authenticate(username, password):
+        user = User.query.filter(User.username == username).first()
+        if user and user.check_password(password):
+            return user
+        else:
+            return False
+
+
     def __repr__(self):
         return "User(id={}, email={}, password={}, admin={}".format(self.id, self.email, self.password, self.admin)
+
+
+class Subjects(db.Model):
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    name = Column(String(25), default='')
+    major = Column(String(25), default='')
+    faculty = Column(String(25), default='')
+
+    def __repr__(self):
+        return "Subject(id={}, name={}, major={}, faculty={}".format(self.id, self.name, self.major, self.faculty)
+
+class Lecture(db.Model):
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    name = Column(String(25), default='')
+    consult = Column(DateTime())
+
+    def __repr__(self):
+        return "Subject(id={}, name={}, major={}, faculty={}".format(self.id, self.name, self.major, self.faculty)
 
 
 class RegistrationForm(Form):
@@ -50,11 +105,27 @@ class RegistrationForm(Form):
     password = PasswordField('Password', [validators.InputRequired(),
                                           validators.EqualTo('confirm', message='Passwords must match')])
     confirm = PasswordField('Repeat Password')
+    #submit = SubmitField('Register')
+
 
 class LoginForm(Form):
     username = StringField('Username', [validators.Length(min=5, max=25), validators.InputRequired()])
     password = PasswordField('Password', [validators.InputRequired(), validators.Length(min=5, max=25)])
-    #todo login przez username lub email
+    remember_me = BooleanField('Remember Me', [validators.DataRequired()], default=True)
+
+    def validate(self):
+        if not super(LoginForm, self).validate():
+            return False
+
+        user = User.authenticate(self.username.data, self.password.data)
+        if not user:
+            self.username.errors.append('Invalid username or password')
+            return False
+
+        return True
+
+#todo login przez username lub email
+
 
 class ChangePassword(Form):
     password = PasswordField('Password', [validators.InputRequired(),
--- views.py
+++ views.py
@@ -6,12 +6,17 @@ from main import bcrypt
 from main import lm
 
 from flask import render_template, redirect, request, flash, Blueprint, session
-from flask.ext.login import LoginManager, login_required, logout_user, login_user
+from flask.ext.login import LoginManager, login_required, logout_user, login_user, current_user
 from models import User, LoginForm, RegistrationForm
+from flask import g
 
 @lm.user_loader
-def load_user(id):
-    return User.query.get(id)
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.before_request
+def before_request():
+    g.user = current_user
 
 @app.route('/', methods=['GET', 'POST'])
 def info():
@@ -21,6 +26,14 @@ def info():
                "<b><a href = '/logout'>click here to log out</a></b>"
     else:
         return render_template('info.html')
+
+@app.route('/search', methods=['GET'])
+def search():
+    pass
+
+@app.route('/profile', methods=['GET'])
+def profile():
+    pass
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -38,23 +51,21 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm(request.form)
-    error = None
-    if request.method == 'POST' and form.validate():
-        user = User.query.filter_by(username=form.username.data.lower()).first()
-        if user:
-            if login_user(user):
+    if request.method == 'POST':
+        form = LoginForm(request.form)
+        if form.validate():
+                login_user(form.user, remember=form.remember_me.data)
                 flash('Logged in successfully.')
                 return redirect('/')
-        else:
-            error = 'Invalid username or password.'
     else:
+        form = LoginForm()
         return render_template('login.html', form=form)
 
 @app.route('/logout', methods=['GET'])
 @login_required
 def logout():
     logout_user()
+    flash('You have been logged out')
     return redirect('/login')
 
 @app.route('/edit-profile', methods=['GET'])
@@ -62,11 +73,62 @@ def logout():
 def edit_profile():
     pass
 
+@app.route('/change-password', methods=['GET', 'POST'])
+@login_required
+def change_password():
+    pass
+
+@app.route('/subjects', methods=['GET'])
+@login_required
+def subjects():
+    '''
+
+    lista wszystkich przedmiotow odpowiednia dla danego studenta kierunku i roku
+
+    '''
+    pass
+
+@app.route('/subjects/<string:name>', methods=['GET'])
+@login_required
+def subject():
+    '''
+
+    info o danym przedmiocie:
+        prowadzący w zależności od grupy laboratoryjnej
+        dni kiedy odbywają się zajęcia
+    '''
+    pass
+
+@app.route('/lectures', methods=['GET'])
+@login_required
+def lectures():
+    '''
+
+    lista wszystkich prowadzących odpowiednich dla danego studenta kierunku i roku
+
+    '''
+    pass
+
+@app.route('/lectures/<string:name>', methods=['GET'])
+@login_required
+def lecture():
+    '''
+
+    info o danym prowadzącym
+        dzien i miejsce konsultacji
+
+    '''
+    pass
+
 @app.route('/plan', methods=['GET', 'POST'])
 @login_required
 def plan():
     pass
 
+
+
 #todo remember me
 #todo login with fb & twitter
 #todo forgot password
+#todo szukaj
+#todo chat
