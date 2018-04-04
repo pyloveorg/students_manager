7c4f2eaadeb885e18226fcda13e444d14047181a
Kamila Urbaniak <kamila.urbaniak26@gmail.com>
2018-04-04 20:12:31 +0200

    ;

--- faculties.py
+++ faculties.py
@@ -5,7 +5,7 @@ from flask_login import login_required
 from models import Subject, Major, Faculty, Year
 
 
-# wszystkie wydziały
+# return all faculties
 @app.route('/faculties/', methods=['GET'])
 @login_required
 def faculties():
@@ -13,31 +13,52 @@ def faculties():
     return render_template('faculty/faculties.html', faculties=faculties)
 
 
-# info o danym wydziale
-@app.route('/faculties/<int:id>', methods=['GET'])
+# return faculty with particular id
+@app.route('/faculties/<int:id1>', methods=['GET'])
 @login_required
-def faculty(id):
-    faculty = Faculty.query.filter(Faculty.id == id).first()
-    return render_template('faculty/faculty.html', faculties=faculties)
+def faculty(id1):
+    faculty = Faculty.query.filter(Faculty.id == id1).first()
+    return render_template('faculty/faculty.html', faculty=faculty)
+
+# add new faculty
+@app.route('/faculties/', methods=['POST'])
+@login_required
+def add_faculty():
+    pass
 
 
-# kierunki dla odpowiedniego wydziału oraz info
-@app.route('/faculties/<int:id>/majors', methods=['GET'])
+# return all majors with particular faculty
+@app.route('/faculties/<int:id1>/majors', methods=['GET'])
 @login_required
-def majors(id):
-    majors = Major.query.filter(Faculty.id == id).all()
+def majors(id1):
+    majors = Major.query.filter(Faculty.id == id1).all()
     return render_template('faculty/majors.html', majors=majors)
 
 
-# roczniki dla odpowiedniego kierunku
-@app.route('/faculties/<int:id1>/majors/<int:id2>/years/', methods=['GET'])
+# return major with particular faculty and id
+@app.route('/faculties/<int:id1>/majors/<int:id2>', methods=['GET'])
+@login_required
+def major(id1):
+    major = Major.query.filter(Faculty.id == id1).first()
+    return render_template('faculty/majors.html', major=major)
+
+
+# add new major with particular faculty
+@app.route('/faculties/<int:id1>/majors', methods=['POST'])
+@login_required
+def add_major(id1):
+    pass
+
+
+# return all years with particular faculty and major
+@app.route('/faculties/<int:id1>/majors/<int:id2>/years', methods=['GET'])
 @login_required
 def years(id1, id2):
     majors = Major.query.filter(Faculty.id == id).all()
     return render_template('faculty/years.html', majors=majors)
 
 
-# rocznik dla odpowiedniego kierunku
+# return year with particular faculty, major and id
 @app.route('/faculties/<int:id1>/majors/<int:id2>/years/<int:id3>', methods=['GET'])
 @login_required
 def year(id1, id2):
@@ -45,32 +66,35 @@ def year(id1, id2):
     return render_template('faculty/majors.html', majors=majors)
 
 
+# add new year with particular faculty and major
+@app.route('/faculties/<int:id1>/major/<int:id2>/years', methods=['POST'])
+@login_required
+def add_years(id1, id2):
+    pass
+
+
 # todo zabezpieczenie przed otwarciem przez studenta, który nie jest z danego wydziału, kierunku, itd.
 # może dekorator?
-# przedmioty dla danego kierunku
+
+# return all subjects with particular faculty, major and year
 @app.route('/faculties/<int:id1>/majors/<int:id2>/years/<int:id3>/subjects', methods=['GET'])
 @login_required
 def subjects(id1, id2, id3):
-    subjects = Subject.query.filter(Faculty.id == id1 and Major.id == id2 and Year.id == id3).all()
-    return render_template('faculty/subjects.html', subjects=subjects)
+    # subjects = Subject.query.filter(Faculty.id == id1 and Major.id == id2 and Year.id == id3).all()
+    # return render_template('faculty/subjects.html', subjects=subjects)
+    pass
 
 
-# info o przedmiocie
+# return subject with particular faculty, major, year and id
 @app.route('/faculty/<int:id1>/major/<int:id2>/years/<int:id3>/subjects/<int:id4>', methods=['GET'])
 @login_required
 def subject(id1, id2, id3, id4):
-    '''
-
-    info o danym przedmiocie:
-        prowadzący w zależności od grupy laboratoryjnej
-        dni kiedy odbywają się zajęcia
-
-    '''
-    subjects = Subject.query.filter(Faculty.id == id1 and Major.id == id2 and Year.id == id3 and Subject.id == id4).all()
-    return render_template('faculty/subjects.html', subjects=subjects)
+    # subjects = Subject.query.filter(Faculty.id == id1 and Major.id == id2 and Year.id == id3 and Subject.id == id4).all()
+    # return render_template('faculty/subjects.html', subjects=subjects)
+    pass
 
 
-#plan zajęć dla danej grupy
+# plan zajęć dla danej grupy
 @app.route('/faculty/<int:id1>/major/<int:id2>/year/<int:id3>/group/<int:id4>/plan', methods=['GET', 'POST'])
 @login_required
 def plan(id1,id2,id3,id4):
@@ -83,14 +107,3 @@ def plan(id1,id2,id3,id4):
     #plan =
     return render_template('plan.html', plan=plan)
 
-
-@app.route('/faculties/<int:id1>/majors/<int:id2>/year/<int:id3>/group/<int:id4>/students', methods=['GET'])
-@login_required
-def redirect_students(id1, id2, id3, id4):
-    return redirect('/students')
-
-
-@app.route('/faculties/<int:id1>/majors/<int:id2>/year/<int:id3>/group/<int:id4>/students/<int:id5>', methods=['GET'])
-@login_required
-def redirect_student(id1, id2, id3, id4, id5):
-    return redirect('/students' + id5)
